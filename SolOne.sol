b9af91ced4f4559fabd44ef326a86974024e0d4d
banjoDevs <pableobenedick033@gmail.com>
2023-04-04 22:39:17 +0800

    first commit

---
+++ SolOne.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract MyContract {
+
+    uint uintVariable;
+    int intVariable;
+    address addressVariable;
+    bool boolVariable;
+
+    function setUint(uint _value) public {
+        uintVariable = _value;
+    }
+
+    function getUint() public view returns (uint) {
+        return uintVariable;
+    }
+
+    function setInt(int _value) public {
+        intVariable = _value;
+     
+    }
+
+    function getInt() public view returns (int) {
+        return intVariable;
+    }
+
+    function setAddress(address _value) public {
+        addressVariable = _value;
+
+    }
+
+    function getAddress() public view returns (address) {
+        return addressVariable;
+    }
+
+    function setBool(bool _value) public {
+        boolVariable = _value;
+    }
+
+    function getBool() public view returns (bool) {
+        return boolVariable;
+    }
+}
