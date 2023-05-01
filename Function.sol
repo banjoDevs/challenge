4c06a032ac17ef1aad266ddd408336d777ad2b2d
banjoDevs <pableobenedick033@gmail.com>
2023-05-01 15:34:56 +0800

    functions

---
+++ Function.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract FunctionSample {
+    function add(uint x, uint y) external pure returns (uint) {
+        return x + y;
+    }
+    function sub(uint x, uint y) external pure returns (uint) {
+        return x - y;
+    }
+    function mul(uint x, uint y) external pure returns (uint) {
+        return x * y;
+    }
+    function div(uint x, uint y) external pure returns (uint) {
+        return x / y;
+    }
+    
+}
